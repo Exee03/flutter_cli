1b57692ff376b67df68f2ada3390e336d57f24d0
Rasul Izuddin <microexee03@gmail.com>
2022-06-29 14:31:33 +0800

    Initial commit

---
+++ lib/extensions.dart
@@ -0,0 +1,5 @@
+library get_cli.extensions;
+
+export 'package:flutter_cli/core/internationalization.dart' show Trans;
+export 'package:flutter_cli/extensions/list.dart';
+export 'package:flutter_cli/extensions/string.dart';
---
+++ lib/flutter_cli.dart
@@ -0,0 +1,3 @@
+export 'package:flutter_cli/commands/impl/commads_export.dart';
+export 'package:flutter_cli/core/generator.dart';
+export 'package:flutter_cli/extensions.dart';
